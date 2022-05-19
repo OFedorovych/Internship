e7e2fbe7c9c69465e18dbb9ca7a513b7015e7fd1
OFedorovych <74896929+OFedorovych@users.noreply.github.com>
2022-05-19 11:55:06 +0300

    Add new scripts for bird contracts

---
+++ scripts/arguments.js
@@ -0,0 +1,5 @@
+module.exports = [
+  "0xEecc2f5e9AD56BEE09fb3B23f53AE73469aEB28b",           //contract with logic
+  "0xf9CeEaeCe36010A76912b5433B5C7295bE57B8C7",           // admin address
+  "0x"                                                    // data
+];
